811f82f9b2c52ed81b74dcfcec6354280d4e5972
cecilianosilva <ciciliano.neto@outlook.com>
2018-08-26 06:19:19 -0300

    Configurando o bootstrap parte 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const gulp = require('gulp');
 const sass = require('gulp-ruby-sass');
+const watch = require('gulp-watch');
+const gulp = require('gulp');
 
 // task para o sass
 gulp.task('sass', function(){
-    return sass('sass/*.sass').pipe(gulp.dest('css'))
+    return sass('sass/**/*.sass').pipe(gulp.dest('css'))
+});
+
+// task para o watch
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass'])
 });
 
 // task defalt gulp
-gulp.task('default', function(){
-
-});
+gulp.task('default', ['sass', 'watch']);
